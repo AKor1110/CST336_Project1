d19c4fc079f08ea238508428069115ea9b043a07
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2018-11-05 22:06:15 +0000

    shopping cart

--- shoppingCart.php
+++ shoppingCart.php
@@ -1,34 +1,34 @@
 <?php
     include "inc/functions.php";
     function displayItems(){ // Displays the users items in the shopping cart
-        //     if (isset($_SESSION['cart'])) {
-        //         echo "<table class='table'>";
-        //         foreach ($_SESSION['cart'] as $item) {
-        //             $itemId = $item['id'];
-        //             $itemQuant = $item['quantity'];
+            if (isset($_SESSION['cart'])) {
+                echo "<table class='table'>";
+                foreach ($_SESSION['cart'] as $item) {
+                    $itemId = $item['id'];
+                    $itemQuant = $item['quantity'];
                     
-        //             echo '<tr>';
+                    //echo '<tr>';
                     
                     
-        //             echo "<tr><img src='". $item['img'] ."'></td>";
-        //             echo "<td><h4>". $item['name'] ."</h4></td>";
-        //             echo "<td><h4>$". $item['price'] ."</h4></td>";
+                    echo "<tr><img src='". $item['img'] ."'></td>";
+                    echo "<td><h4>". $item['name'] ."</h4></td>";
+                    echo "<td><h4>$". $item['price'] ."</h4></td>";
                     
-        //             echo "<form method='post'>";
-        //             echo "<input type='hidden' name='itemId' value='$itemId'>";
-        //             echo "<td><input type='text' name='update' class='form-control' placeHolder='$itemQuant'></td>";
-        //             echo '<td><button class="btn btn-danger">Update</button></td>';
-        //             echo '</form>';
+                    echo "<form method='post'>";
+                    echo "<input type='hidden' name='itemId' value='$itemId'>";
+                    echo "<td><input type='text' name='update' class='form-control' placeHolder='$itemQuant'></td>";
+                    echo '<td><button class="btn btn-danger">Update</button></td>';
+                    echo '</form>';
                     
-        //             echo "<form method='post'>";
-        //             echo "<input type='hidden' name='removeId' value='$itemId'>";
-        //             echo '<td><button class="btn btn-danger">Remove</button></td>';
-        //             echo '</form>';
+                    echo "<form method='post'>";
+                    echo "<input type='hidden' name='removeId' value='$itemId'>";
+                    echo '<td><button class="btn btn-danger">Remove</button></td>';
+                    echo '</form>';
                     
-        //             echo "</tr>";
-        //     }
-        //     echo "</table>";
-        // }
+                    echo "</tr>";
+            }
+            echo "</table>";
+        }
     }
     
     function clearItems(){ // Clears all the items currently in the shopping cart
@@ -55,7 +55,9 @@
             <form method="get">
               <button type="submit" value="Submit" name="remove">Remove all</button>
             </form>
-            <?php clearItems(); ?>
+            <?php 
+            displayItems();
+            clearItems(); ?>
         </div>
     </body>
 </html>
